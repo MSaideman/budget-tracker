e54c7f3648293faf2424efc9316733afe3d92cd4
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-10-21 05:58:41 -0700

    added db file

--- Develop/public/serviceworker.js
+++ Develop/public/serviceworker.js
@@ -1,8 +1,9 @@
 const FILES_TO_CACHE = [
-    "/", "/index.html","index.js", "/style.css"];
+    "/", "/index.html","db.js","index.js", "/style.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
 
 // install
 self.addEventListener("install", function (evt) {
